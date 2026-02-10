e449179852a19ee9fb9fe4c8bf106eb38ea760db
Gabriel Rodrigues <meu.negocio.gr@gmail.com>
2026-02-10 10:20:28 -0300

    Conhecendo Java

---
+++ Aula1/Aula Inaugural/src/Variaveis.java
@@ -0,0 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Variaveis {
+    public static void main(String[] args) {
+
+        int idade = 35;
+        double altura = 1.80;
+        double peso = 85.67;
+
+        char gereno = 'F'; //Caracteres - ''
+        String nome = "Gabriel"; //String - ""
+
+        System.out.println("Nome: " + nome + "\nIdade: " + idade + "\nAltura: " + altura + "\nPeso: " + peso + "\nGenero: " + gereno);
+
+    }
+}
